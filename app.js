98609fb0bd239d802113300317474110eca40aa2
abhinay-kumar-tippani <tippaniabhinay@gmail.com>
2025-10-22 14:06:57 +0530

    added bootstrap css

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ main()
     .then(console.log("Connected to DB"))
     .catch(err => { console.log(err) });
 
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
